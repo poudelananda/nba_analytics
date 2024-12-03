72809312bf64744a3892ccc5ab390d994a12ee5b
Ananda <apoudel53@gmail.com>
2024-12-03 10:33:42 -0500

    minor-flask-updates

--- server.py
+++ server.py
@@ -2,8 +2,10 @@ from flask import Flask, render_template, url_for, request, jsonify
 import json
 import pandas as pd
 import player_position_scraper
+import nba_analytics
 
 app = Flask(__name__)
+
 
 @app.route('/', methods = ['POST', 'GET'])
 def homePage():
@@ -11,9 +13,21 @@ def homePage():
     names = df['PLAYER_NAME']
     return render_template('homepage.html', names = names)
 
+@app.route('/player-data', methods = ['POST', 'GET'])
+def player_data():
+    player_name = request.form.get("player-names")
+    # inner_html = data.get('innerHTML')
+    # player_name_json = jsonify(inner_html)
+    # return render_template('player-performance.html', player_name = inner_html)
+    player_df = nba_analytics.player_stats(player_name)
+    player_df_json = player_df[['PTS', 'AST', 'REB']].describe().to_json()
+    return player_df_json
+
+
 @app.route('/player-performance', methods = ['POST', 'GET'])
 def player_performance():
-    return render_template('player-performance.html')
+    player_data = player_data()
+    return render_template('player-performance.html', data = player_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ static/tabulate.js
@@ -0,0 +1,10 @@
+function table(viz_container, tbl_container, player_json_data) {
+    viz_container = document.getElementById('viz-container')
+    tbl_container = document.getElementById('player-summary')
+
+    key_vals = Object.keys(player_json_data)
+    console.log(key_vals)
+
+    
+
+}
